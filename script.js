096056ce36bfbc9b278a8a003cd09d212e953ac3
Geothon <100970469+Geothon@users.noreply.github.com>
2024-09-11 01:46:18 +0000

    fixed i hope please

--- script.js
+++ script.js
@@ -40,21 +40,21 @@ aichoice= "sissors"
  }
 
  function game() {
-if(playerchoice==rock && aichoice==rock) {console.log ("tie")}
+if(playerchoice == rock && aichoice==rock) {console.log("tie")}
  
-else if(playerchoice==rock && aichoice==paper) {console.log ("ai win")}
+else if(playerchoice == rock && aichoice == paper) {console.log("ai win")}
 
-else if(playerchoice==rock && aichoice==sissors) {console.log ("player win")}
+else if(playerchoice == rock && aichoice == sissors) {console.log("player win")}
 
-else if(playerchoice==paper && aichoice==rock) {console.log ("player win")}
+else if(playerchoice == paper && aichoice == rock) {console.log("player win")}
 
-else if(playerchoice==sissors && aichoice==rock) {console.log ("ai win")}
+else if(playerchoice == sissors && aichoice == rock) {console.log("ai win")}
 
-else if(playerchoice==sissors && aichoice==paper) {console.log ("player win")}
+else if(playerchoice == sissors && aichoice == paper) {console.log("player win")}
 
-else if(playerchoice==paper && aichoice==sissors) {console.log ("ai win")}
+else if(playerchoice == paper && aichoice == sissors) {console.log("ai win")}
 
-else if(playerchoice==paper && aichoice==paper) {console.log ("tie")}
+else if(playerchoice == paper && aichoice == paper) {console.log("tie")}
 
-else if(playerchoice==sissors && aichoice==sissors) {console.log ("tie")}
+else if(playerchoice == sissors && aichoice == sissors) {console.log("tie")}
 };
