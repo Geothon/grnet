3742f3d9dd98f37c21212688ed5a321db1914829
Geothon <100970469+Geothon@users.noreply.github.com>
2024-09-12 00:49:38 +0000

    done.

--- script.js
+++ script.js
@@ -26,40 +26,38 @@ function getRandomInt(max) {
 
 function newNum() {
   number = getRandomInt(3)
-}
 
-console.log(number)
+  if(number == 1) {
+  aichoice = "rock"    
+  }
 
-if(number==1) {
-aichoice = "rock"    
-}
-
- else if(number==2) {
-aichoice = "paper"
+ else if(number == 2) {
+  aichoice = "paper"
  }
 
- else if(number==3) {
-aichoice= "sissors"
+ else if(number == 3) {
+  aichoice = "sissors"
  }
+}
 
  function game() {
-if(playerchoice == rock && aichoice==rock) {console.log("tie")}
+if(playerchoice == "rock" && aichoice =="rock") {console.log("tie")}
  
-else if(playerchoice == rock && aichoice == paper) {console.log("ai win")}
+else if(playerchoice == "rock" && aichoice == "paper") {console.log("ai win")}
 
-else if(playerchoice == rock && aichoice == sissors) {console.log("player win")}
+else if(playerchoice == "rock" && aichoice == "sissors") {console.log("player win")}
 
-else if(playerchoice == paper && aichoice == rock) {console.log("player win")}
+else if(playerchoice == "paper" && aichoice == "rock") {console.log("player win")}
 
-else if(playerchoice == sissors && aichoice == rock) {console.log("ai win")}
+else if(playerchoice == "sissors" && aichoice == "rock") {console.log("ai win")}
 
-else if(playerchoice == sissors && aichoice == paper) {console.log("player win")}
+else if(playerchoice == "sissors" && aichoice == "paper") {console.log("player win")}
 
-else if(playerchoice == paper && aichoice == sissors) {console.log("ai win")}
+else if(playerchoice == "paper" && aichoice == "sissors") {console.log("ai win")}
 
-else if(playerchoice == paper && aichoice == paper) {console.log("tie")}
+else if(playerchoice == "paper" && aichoice == "paper") {console.log("tie")}
 
-else if(playerchoice == sissors && aichoice == sissors) {console.log("tie")}
+else if(playerchoice == "sissors" && aichoice == "sissors") {console.log("tie")}
 
 else {console.log("Error")}
 };
